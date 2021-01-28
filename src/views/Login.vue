f12b2d151bb37836b82a0ae15a1be6a260292523
Hasan Sheikh <sheikharthur@gmail.com>
2021-01-28 23:51:36 +0600

    added loader in login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,9 +2,9 @@
     <div class="main">
         <div class="card login-form">
             <div class="card-body" v-if="showFrom=='login'">
-                <h3 class="card-title text-center">Log in to crud app</h3>
+                <h3 class="card-title text-center">Log in to Vuejs crud</h3>
                 <div class="alert alert-danger" role="alert" v-if="errorMessage.length > 0">
-                    {{errorMessage}}
+                   <li v-for="(error,ind) in errorMessage" :key="ind">{{ error }}</li>
                 </div>
                 <div class="alert alert-success" role="alert" v-if="successMessage != null">
                     {{ successMessage }}
@@ -20,7 +20,7 @@
                             <input v-model="login.password" type="password" class="form-control form-control-sm" id="password">
                         </div>
                         <div class="d-grid">
-                            <button type="submit" class="btn btn-primary">Sign in</button>
+                            <button type="submit" :disabled="isLoading" class="btn btn-primary">Sign in</button>
                         </div>
                         
                         <div class="footer">
@@ -30,7 +30,7 @@
                 </div>
             </div>
             <div class="card-body" v-if="showFrom=='registration'">
-                <h3 class="card-title text-center">Registration to crud app</h3>
+                <h3 class="card-title text-center">Registration to vuejs</h3>
                 <div class="card-text">
                     <div class="alert alert-danger" role="alert" v-if="errorMessage.length > 0">
                         <li v-for="(error,ind) in errorMessage" :key="ind">{{ error }}</li>
@@ -52,7 +52,7 @@
                             <input v-model="registration.password" type="password" class="form-control form-control-sm" id="password">
                         </div>
                         <div class="d-grid">
-                            <button type="submit" class="btn btn-primary">Registration</button>
+                            <button type="submit" :disabled="isLoading" class="btn btn-primary">Registration</button>
                         </div>
                         
                         <div class="footer">
@@ -78,27 +78,36 @@ export default {
             password: ''
         },
         errorMessage: [],
-        successMessage: null
+        successMessage: null,
+        isLoading: false
     }),
     methods: {
         userRegistration: function() {
+            this.isLoading = true;
             this.errorMessage = [];
             this.successMessage = null;
             this.$store.dispatch('auth/registration', this.registration).then((res) => {
                 this.successMessage = res;
                 this.clearForm();
+                this.isLoading = false;
             }).catch((error) => {
                 this.errorMessage = error;
+                this.isLoading = false;
             })
         },
 
         userLogin: function() {
+            this.isLoading = true;
             this.errorMessage = [];
             this.successMessage = null;
             this.$store.dispatch('auth/login', this.login).then(response => {
+                this.isLoading = false;
                 this.$router.push('/');
             })
-            .catch(error => this.errorMessage = error);
+            .catch(error => {
+                this.errorMessage = error;
+                this.isLoading = false;
+            });
         },
 
         clearForm: function() {
